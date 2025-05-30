114db4a8c65d9b0bf8168c719fbdcb4bf05073a4
xHak1m <ivxhak1m@gmail.com>
2025-05-30 16:04:01 +0800

    Create 01 - First Programming.cpp

---
+++ docs/resources/01 - First Programming.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Include the input/output library
+
+using namespace std; // Use the standard namespace
+
+int main() {
+	cout << "Hello, World" << endl; // Print to the console
+	return 0; // Indicate the program ended successfully
+}
